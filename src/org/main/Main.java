1cc5bc5d502a80a389025a7fe53d05d1761bf4b5
mearvk <mearvk@mearvk.us>
2022-10-05 14:53:31 -0400

    PhD Work; NC State (continuing)

--- src/org/main/Main.java
+++ src/org/main/Main.java
@@ -9,8 +9,6 @@ import org.system.publishment.SystemPublishment;
 
 public class Main
 {
-	PublishmentContext publishment;
-
 	public static void main(String... args)
 	{
 		SystemContext context = new SystemContext
@@ -26,30 +24,30 @@ public class Main
 
 		OrdenmentContext ordenment = new OrdenmentContext
 		(
-			new OrdenmentPublishment(0x1, new org.system.servers.http.system.System("{branch name}")),
-			new OrdenmentPublishment(0x2, new org.system.servers.http.firewall.Firewall("{firewall}")),
-			new OrdenmentPublishment(0x3, new org.system.servers.http.authentication.Authenticator("{authentication}")),
-			new OrdenmentPublishment(0x4, new org.system.servers.http.inspection.PacketInspector("{packet inspection}")),
-			new OrdenmentPublishment(0x5, new org.system.servers.http.logging.Logging("{dns/ip/port logging}")),
-			new OrdenmentPublishment(0x6, new org.system.servers.http.exceptions.Exception("{exception logging}")),
-			new OrdenmentPublishment(0x7, new org.system.servers.http.parsing.Parser("{parser}")),
-			new OrdenmentPublishment(0x8, new org.system.servers.http.nio.FileHandler("{file handler}")),
-			new OrdenmentPublishment(0x9, new org.system.servers.http.output.OutputHandler("{output handler}"))
+			new OrdenmentPublishment(0x0001, new org.system.servers.http.system.System("{branch name}")),
+			new OrdenmentPublishment(0x0002, new org.system.servers.http.firewall.Firewall("{firewall}")),
+			new OrdenmentPublishment(0x0003, new org.system.servers.http.authentication.Authenticator("{authentication}")),
+			new OrdenmentPublishment(0x0004, new org.system.servers.http.inspection.PacketInspector("{packet inspection}")),
+			new OrdenmentPublishment(0x0005, new org.system.servers.http.logging.Logging("{dns/ip/port logging}")),
+			new OrdenmentPublishment(0x0006, new org.system.servers.http.exceptions.Exception("{exception logging}")),
+			new OrdenmentPublishment(0x0007, new org.system.servers.http.parsing.Parser("{parser}")),
+			new OrdenmentPublishment(0x0008, new org.system.servers.http.nio.FileHandler("{file handler}")),
+			new OrdenmentPublishment(0x0009, new org.system.servers.http.output.OutputHandler("{output handler}"))
 		);
 
 		PublishmentContext publishment = new PublishmentContext
 		(
 			new SystemContext.SystemHTTPServer("mearvk.us","http-publishment-server", 19998),
 			new SystemContext.SystemHTTPServer.Messaging.Publishment("POST", "{program-name}/publishments", "http-server-parametry"),
-			new SystemPublishment.SystemPartParametry(0x01, 0x001,"//documents/document-000.xml", new SystemContext.SamRegistry("//documents/registry-000.xml"), new SystemContext.Username("jndi", "mearvk.us", "{username}"), new SystemContext.Password("jndi", "mearvk.us", "{password}")),
-			new SystemPublishment.SystemPartParametry(0x02, 0x001,"//documents/document-001.xml", new SystemContext.SamRegistry("//documents/registry-001.xml"), new SystemContext.Username("jndi", "mearvk.us", "{username}"), new SystemContext.Password("jndi", "mearvk.us", "{password}")),
-			new SystemPublishment.SystemPartParametry(0x03,0x001,"//documents/document-002.xml", new SystemContext.SamRegistry("//documents/registry-002.xml"), new SystemContext.Username("jndi", "mearvk.us", "{username}"), new SystemContext.Password("jndi", "mearvk.us", "{password}")),
-			new SystemPublishment.SystemPartParametry(0x04,0x001,"//documents/document-003.xml", new SystemContext.SamRegistry("//documents/registry-003.xml"), new SystemContext.Username("jndi", "mearvk.us", "{username}"), new SystemContext.Password("jndi", "mearvk.us", "{password}")),
-			new SystemPublishment.SystemPartParametry(0x05,0x001,"//documents/document-004.xml", new SystemContext.SamRegistry("//documents/registry-004.xml"), new SystemContext.Username("jndi", "mearvk.us", "{username}"), new SystemContext.Password("jndi", "mearvk.us", "{password}")),
-			new SystemPublishment.SystemPartParametry(0x06,0x001,"//documents/document-005.xml", new SystemContext.SamRegistry("//documents/registry-005.xml"), new SystemContext.Username("jndi", "mearvk.us", "{username}"), new SystemContext.Password("jndi", "mearvk.us", "{password}")),
-			new SystemPublishment.SystemPartParametry(0x07,0x001,"//documents/document-006.xml", new SystemContext.SamRegistry("//documents/registry-006.xml"), new SystemContext.Username("jndi", "mearvk.us", "{username}"), new SystemContext.Password("jndi", "mearvk.us", "{password}")),
-			new SystemPublishment.SystemPartParametry(0x08,0x001,"//documents/document-007.xml", new SystemContext.SamRegistry("//documents/registry-007.xml"), new SystemContext.Username("jndi", "mearvk.us", "{username}"), new SystemContext.Password("jndi", "mearvk.us", "{password}")),
-			new SystemPublishment.SystemPartParametry(0x09,0x001,"//documents/document-008.xml", new SystemContext.SamRegistry("//documents/registry-008.xml"), new SystemContext.Username("jndi", "mearvk.us", "{username}"), new SystemContext.Password("jndi", "mearvk.us", "{password}"))
+			new SystemPublishment.SystemPartParametry(0x0001, 0x0001,"//documents/document-000.xml", new SystemContext.SamRegistry("//documents/registry-000.xml"), new SystemContext.Username("jndi", "mearvk.us", "{username}"), new SystemContext.Password("jndi", "mearvk.us", "{password}")),
+			new SystemPublishment.SystemPartParametry(0x0002, 0x0001,"//documents/document-001.xml", new SystemContext.SamRegistry("//documents/registry-001.xml"), new SystemContext.Username("jndi", "mearvk.us", "{username}"), new SystemContext.Password("jndi", "mearvk.us", "{password}")),
+			new SystemPublishment.SystemPartParametry(0x0003,0x0001,"//documents/document-002.xml", new SystemContext.SamRegistry("//documents/registry-002.xml"), new SystemContext.Username("jndi", "mearvk.us", "{username}"), new SystemContext.Password("jndi", "mearvk.us", "{password}")),
+			new SystemPublishment.SystemPartParametry(0x0004,0x0001,"//documents/document-003.xml", new SystemContext.SamRegistry("//documents/registry-003.xml"), new SystemContext.Username("jndi", "mearvk.us", "{username}"), new SystemContext.Password("jndi", "mearvk.us", "{password}")),
+			new SystemPublishment.SystemPartParametry(0x0005,0x0001,"//documents/document-004.xml", new SystemContext.SamRegistry("//documents/registry-004.xml"), new SystemContext.Username("jndi", "mearvk.us", "{username}"), new SystemContext.Password("jndi", "mearvk.us", "{password}")),
+			new SystemPublishment.SystemPartParametry(0x0006,0x0001,"//documents/document-005.xml", new SystemContext.SamRegistry("//documents/registry-005.xml"), new SystemContext.Username("jndi", "mearvk.us", "{username}"), new SystemContext.Password("jndi", "mearvk.us", "{password}")),
+			new SystemPublishment.SystemPartParametry(0x0007,0x0001,"//documents/document-006.xml", new SystemContext.SamRegistry("//documents/registry-006.xml"), new SystemContext.Username("jndi", "mearvk.us", "{username}"), new SystemContext.Password("jndi", "mearvk.us", "{password}")),
+			new SystemPublishment.SystemPartParametry(0x0008,0x0001,"//documents/document-007.xml", new SystemContext.SamRegistry("//documents/registry-007.xml"), new SystemContext.Username("jndi", "mearvk.us", "{username}"), new SystemContext.Password("jndi", "mearvk.us", "{password}")),
+			new SystemPublishment.SystemPartParametry(0x0009,0x0001,"//documents/document-008.xml", new SystemContext.SamRegistry("//documents/registry-008.xml"), new SystemContext.Username("jndi", "mearvk.us", "{username}"), new SystemContext.Password("jndi", "mearvk.us", "{password}"))
 		);
 	}
 }
